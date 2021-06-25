7ce0fa237b62994835e0fedd17c6cc80f1ae6d61
Alexandra Hampau <alehampau@gmail.com>
2021-06-25 12:18:42 +0300

    Commit autentification form(still not working)

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,3 +177,132 @@
   });
 
 })(jQuery);
+
+var mymap = L.map('mapid').setView([45.7936, -24.1213], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+
+var circle = L.circle([45.7936, -24.1213], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 500
+}).addTo(mymap);
+
+var polygon = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047]
+]).addTo(mymap);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup()
+    .setLatLng([45.7936
+      , -24.1213
+    ])
+    .setContent("I am a standalone popup.")
+    .openOn(mymap);
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+  
+mymap.on('click', onMapClick);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
+
+//Autentification pop-up*/
+/*
+ *
+ * login-register modal
+ * Autor: Creative Tim
+ * Web-autor: creative.tim
+ * Web script: http://creative-tim.com
+ * 
+ */
+function showRegisterForm(){
+    $('.loginBox').fadeOut('fast',function(){
+        $('.registerBox').fadeIn('fast');
+        $('.login-footer').fadeOut('fast',function(){
+            $('.register-footer').fadeIn('fast');
+        });
+        $('.modal-title').html('Register with');
+    }); 
+    $('.error').removeClass('alert alert-danger').html('');
+       
+}
+function showLoginForm(){
+    $('#loginModal .registerBox').fadeOut('fast',function(){
+        $('.loginBox').fadeIn('fast');
+        $('.register-footer').fadeOut('fast',function(){
+            $('.login-footer').fadeIn('fast');    
+        });
+        
+        $('.modal-title').html('Login with');
+    });       
+     $('.error').removeClass('alert alert-danger').html(''); 
+}
+
+function openLoginModal(){
+    showLoginForm();
+    setTimeout(function(){
+        $('#loginModal').modal('show');    
+    }, 230);
+    
+}
+function openRegisterModal(){
+    showRegisterForm();
+    setTimeout(function(){
+        $('#loginModal').modal('show');    
+    }, 230);
+    
+}
+
+function loginAjax(){
+    /*   Remove this comments when moving to server
+    $.post( "/login", function( data ) {
+            if(data == 1){
+                window.location.replace("/home");            
+            } else {
+                 shakeModal(); 
+            }
+        });
+    */
+
+/*   Simulate error message from the server   */
+     shakeModal();
+}
+
+function shakeModal(){
+    $('#loginModal .modal-dialog').addClass('shake');
+             $('.error').addClass('alert alert-danger').html("Invalid email/password combination");
+             $('input[type="password"]').val('');
+             setTimeout( function(){ 
+                $('#loginModal .modal-dialog').removeClass('shake'); 
+    }, 1000 ); 
+}
+
+   
+function signin(){
+  document.getElementById('container_popup').style.visibility='visible';
+}
+//End of Autentification pop-up//
+
