b2dcfeb5c76aa57f9cb5741c77995f66f8727a95
V Bota <VBota1@users.noreply.github.com>
2021-06-27 08:09:42 +0300

    removed unused items

--- sez.js
+++ sez.js
@@ -11,17 +11,17 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 var marker = L.marker([45.7489, 21.2087 -0.09]).addTo(mymap);
 
 
-var popup = new L.marker();
+// var popup = new L.marker();
 
-function onMapClick(e) {
-    marker
-        .bindPopup("Ai selectat ").openPopup()
-        .setLatLng(e.latlng)
-        // .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
-}
+// function onMapClick(e) {
+//     marker
+//         .bindPopup("Ai selectat ").openPopup()
+//         .setLatLng(e.latlng)
+//         // .setContent("You clicked the map at " + e.latlng.toString())
+//         .openOn(mymap);
+// }
 
-mymap.on('click', onMapClick);
+// mymap.on('click', onMapClick);
 
 
 var requestOptions = {
@@ -34,8 +34,6 @@ fetch("https://sibiuinventoryapimanager.azure-api.net/v1/Pins", requestOptions)
   .then(results=> {
       for(let i = 0; i < results.data.length; i++) {
         L.marker([results.data[i].gpsCoordX, results.data[i].gpsCoordY]).addTo(mymap);
-        console.log(results.data[i].gpsCoordX);
-            console.log(results.data[i].gpsCoordY);
       }
   })
   .catch(error => console.log('error', error));
