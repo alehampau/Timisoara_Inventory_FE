6c75785310da583da7a7ff12f25b8c96c8282080
V Bota <VBota1@users.noreply.github.com>
2021-06-27 06:16:55 +0300

    implemented put and delete

--- administrare.js
+++ administrare.js
@@ -9,20 +9,9 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   zoomOffset: -1
 }).addTo(mymap);
 
-// var marker = L.marker([45.7489, 21.2087 -0.09]).addTo(mymap);
-
-
-var popup = new L.marker();
-
-function onMapClick(e) {
-    marker
-        .bindPopup("<button> bla bla </button>").openPopup()
-        .setLatLng(e.latlng)
-        // .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
-}
-
 mymap.on('click', onMapClick);
+
+var markers = []
 
 //Functionality for pins
 function getAllPins() {
@@ -31,51 +20,124 @@ function getAllPins() {
     redirect: 'follow'
   };
   
-  fetch("https://sibiuinventoryapimanager.azure-api.net/v1/Pins", requestOptions)
-    .then(response => response.json())
-    .then(results=> {
-        for(let i = 0; i < results.data.length; i++) {
-          L.marker([results.data[i].gpsCoordX, results.data[i].gpsCoordY]).addTo(mymap);
-        }
-    })
-    .catch(error => console.log('error', error));
+fetch("https://sibiuinventoryapimanager.azure-api.net/v1/Pins", requestOptions)
+  .then(response => response.json())
+  .then(results=> {
+    for(let i = 0; i < results.data.length; i++) {
+      var newMarker = L.marker([results.data[i].gpsCoordX, results.data[i].gpsCoordY])
+      .bindPopup("<div> <b>Descriere: </b>"+results.data[i].description+"</div><hr>"+
+      "<a href='administrare.html#editForm' class='btn btn-info btn-fill btn-wd'>Sterge marker</a>"+
+      "<a href='administrare.html#editForm' class='btn btn-info btn-fill btn-wd'>Modifica descriere</a>")
+      .addTo(mymap);
+      newMarker.addEventListener('click',logPosition);
+      markers.push(results.data[i]);
+    }
+  })
+  .catch(error => console.log('error', error));
+}
+
+function logPosition(e) {
+  let coordinates = e.latlng;
+  var latElement = document.getElementById('latitude');
+  latElement.value = coordinates.lat;
+  latElement.style.border = '1px solid black';      
+  var lngElement = document.getElementById('longitude');
+  lngElement.value = coordinates.lng;        
+  lngElement.style.border = '1px solid black';   
+
+  for(let i = 0; i < markers.length; i++) {
+    if (markers[i].gpsCoordX == latElement.value && markers[i].gpsCoordY == lngElement.value) {
+      document.getElementById('pinDescription').value = markers[i].description;
+      break;
+    }
+  }  
+}
+
+function removePin() {
+  var latElement = document.getElementById('latitude');
+  var lngElement = document.getElementById('longitude');
+  for(let i = 0; i < markers.length; i++) {
+    if (markers[i].gpsCoordX == latElement.value && markers[i].gpsCoordY == lngElement.value) {
+      var requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow',
+        mode: 'cors'
+      };
+      
+      fetch("https://sibiuinventoryapimanager.azure-api.net/v1/Pins/"+markers[i].id, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+      break;
+    }
+  }
+  location.reload();  
+}
+
+function updatePin() {
+  var latElement = document.getElementById('latitude');
+  var lngElement = document.getElementById('longitude');
+  for(let i = 0; i < markers.length; i++) {
+    if (markers[i].gpsCoordX == latElement.value && markers[i].gpsCoordY == lngElement.value) {
+      var description = document.getElementById('pinDescription').value;
+      var foundMarker = markers[i];
+      foundMarker.description = description;
+
+      var requestOptions = {
+        method: 'PUT',
+        headers: {
+          "Content-Type": "application/json", 
+          "Access-Control-Allow-Origin": '*'},
+        mode: 'cors',
+        body: JSON.stringify(foundMarker),
+        redirect: 'follow'
+      };    
+
+      fetch("https://sibiuinventoryapimanager.azure-api.net/v1/Pins/"+foundMarker.id, requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+
+      break;
+    }
+  }
+  location.reload();  
 }
 
 getAllPins();
 
-    function onMapClick(e) {
-        let coordinates = e.latlng;
-        var latElement = document.getElementById('latitude');
-        latElement.value = coordinates.lat;
-        latElement.style.border = '1px solid black';      
-        var lngElement = document.getElementById('longitude');
-        lngElement.value = coordinates.lng;        
-        lngElement.style.border = '1px solid black';      
+function onMapClick(e) {
+    let coordinates = e.latlng;
+    var latElement = document.getElementById('latitude');
+    latElement.value = coordinates.lat;
+    latElement.style.border = '1px solid black';      
+    var lngElement = document.getElementById('longitude');
+    lngElement.value = coordinates.lng;        
+    lngElement.style.border = '1px solid black';      
 
-        L.popup()
-        .setLatLng(coordinates)
-        .setContent("<a href='administrare.html#editForm' id='saveBtn' class='btn btn-info btn-fill btn-wd'>Salveaza marker</a>")
-        .openOn(mymap);
-    }
+    L.popup()
+    .setLatLng(coordinates)
+    .setContent("<a href='administrare.html#editForm' id='saveBtn' class='btn btn-info btn-fill btn-wd'>Salveaza marker</a>")
+    .openOn(mymap);
+}
 
-    
-      
-      fetch("https://sibiuinventoryapimanager.azure-api.net/v1/PinTypes", {
-        method: 'GET',
-        redirect: 'follow'
-      })
-      .then(response => response.json())
-      .then(results=> {
-          var selector = document.getElementById("categoryMaster");
-          for(let i = 0; i < results.data.length; i++) {
-            var option = document.createElement("option");
-            option.text = results.data[i].id + "_" + results.data[i].name;
-            selector.add(option);
-            
-          }
-      })
-      .catch(error => console.log('error', error));
-      
+
+  
+  fetch("https://sibiuinventoryapimanager.azure-api.net/v1/PinTypes", {
+    method: 'GET',
+    redirect: 'follow'
+  })
+  .then(response => response.json())
+  .then(results=> {
+      var selector = document.getElementById("categoryMaster");
+      for(let i = 0; i < results.data.length; i++) {
+        var option = document.createElement("option");
+        option.text = results.data[i].id + "_" + results.data[i].name;
+        selector.add(option);
+        
+      }
+  })
+  .catch(error => console.log('error', error));
 
 function handleSubmit(event) {
   event.preventDefault();
