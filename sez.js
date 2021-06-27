fcffe9f382feb63b298a33315a480c8ab1b7184c
V Bota <VBota1@users.noreply.github.com>
2021-06-27 08:33:58 +0300

    added functions for issues

--- sez.js
+++ sez.js
@@ -23,6 +23,7 @@ var marker = L.marker([45.7489, 21.2087 -0.09]).addTo(mymap);
 
 // mymap.on('click', onMapClick);
 
+var markers = []
 
 var requestOptions = {
   method: 'GET',
@@ -33,10 +34,29 @@ fetch("https://sibiuinventoryapimanager.azure-api.net/v1/Pins", requestOptions)
   .then(response => response.json())
   .then(results=> {
       for(let i = 0; i < results.data.length; i++) {
-        L.marker([results.data[i].gpsCoordX, results.data[i].gpsCoordY])
+        var newMarker = L.marker([results.data[i].gpsCoordX, results.data[i].gpsCoordY])
         .bindPopup("<div> <b>Description: </b>"+results.data[i].description+"</div><hr>"+
         "<a href='administrare.html#editForm' class='btn btn-info btn-fill btn-wd''>Report an issue</a>")
         .addTo(mymap);
+        newMarker.addEventListener('click',logPosition);
+        markers.push(results.data[i]);        
       }
   })
   .catch(error => console.log('error', error));
+
+function logPosition(e) {
+  let coordinates = e.latlng;
+  var latElement = document.getElementById('latitude');
+  latElement.value = coordinates.lat;
+  latElement.style.border = '1px solid black';      
+  var lngElement = document.getElementById('longitude');
+  lngElement.value = coordinates.lng;        
+  lngElement.style.border = '1px solid black';   
+
+  for(let i = 0; i < markers.length; i++) {
+    if (markers[i].gpsCoordX == latElement.value && markers[i].gpsCoordY == lngElement.value) {
+      document.getElementById('pinDescription').value = markers[i].description;
+      break;
+    }
+  }  
+}  
